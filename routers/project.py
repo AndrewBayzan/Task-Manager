47d4f7d3a6c909c305b20b66995e6d18cb4d35ac
Andrew <samara.enjoyer@gmail.com>
2026-06-06 02:21:54 +0300

    Added create project

--- routers/project.py
+++ routers/project.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Request, Cookie, Depends
+from fastapi import APIRouter, Request, Cookie, Depends, Form, HTTPException
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse, RedirectResponse
 
 from db import get_db
-from models import User, Project, UserProject
+from models import User, Project, UserProject, RoleEnum
 
 router = APIRouter(prefix="/projects", tags=["Projects page"])
 templates = Jinja2Templates(directory="templates")
@@ -34,10 +34,22 @@ def projects(
         "username" : user.username
     })
 
-# @router.post("/create_project")
-# def create_project(
-#     request: Request,
-#     user_id: int | None = Cookie(default=None),
-#     db: Session = Depends(get_db)
-# ):
+@router.post("/create_project")
+def create_project(
+    project_name: str = Form(...),
+    user_id: int | None = Cookie(default=None),
+    db: Session = Depends(get_db)
+):
+    if not user_id:
+        return RedirectResponse(url="/auth/login", status_code=303)
     
+    project = Project(project_name=project_name, owner_id=user_id)
+    db.add(project)
+    db.commit()
+    db.refresh(project)
+
+    user_project = UserProject(user_id=user_id, project_id=project.id, role=RoleEnum.admin)
+    db.add(user_project)
+    db.commit()
+    
+    return RedirectResponse(url="/projects", status_code=303)
